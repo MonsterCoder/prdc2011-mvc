881482501491a1a6dbda2a3b30aefac8f63ec875
MonsterCoder <monstercoder@gmail.com>
2011-08-15 21:54:01 -0500

    hide back button when session tab is clicked

--- app/controllers/SessionController.js
+++ app/controllers/SessionController.js
@@ -1,12 +1,17 @@
 prdc.controllers.SessionController = new Ext.Controller ({
 	Index: function(param){
 		sessionStore.clearFilter();
+		var showback = false;
 		if (param.filter){
 				for(var field in param.filter){
 					sessionStore.filter(field,param.filter[field],true);
 				}			
+				showback = true;
 		}
 		prdc.views.viewport.setActiveItem(1);
+		
+		//hide back button
+		prdc.views.viewport.getActiveItem().getActiveItem().getDockedItems()[0].items.first().setVisible(showback);
 	},
 	
 	Show: function(param) {
