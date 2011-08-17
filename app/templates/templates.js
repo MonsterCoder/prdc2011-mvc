c25aa78504041e4ac8d6a90d9ccd71ebbc322be1
MonsterCoder <monstercoder@gmail.com>
2011-08-16 21:52:49 -0500

    merged quan's patch

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -1,8 +1,26 @@
 prdc.views.Templates = {};
 
+
+/*speakerTpl = new Ext.XTemplate(
+	'<table>',
+	'<img src="http://prairiedevcon.com/Content/imgs/speakers/{picture}" alt="{picture}" style="width:60px;height:60px;" />',
+	' <div><h3>{name}, {location}</h3><h4>{company}</h4></div>',
+	'</table>'
+);*/
+
+speakerListTpl = Ext.XTemplate.from('speakerListTpl');
+speakerDetailTpl = Ext.XTemplate.from('speakerDetailTpl');
+sessionItemTpl = Ext.XTemplate.from('sessionItemTpl');
+sessionDetailTpl = Ext.XTemplate.from('sessionDetailTpl');
+	
+
+
 Ext.apply(prdc.views.Templates, {
-        speakerItem: '<img src="http://prairiedevcon.com/Content/imgs/speakers/{picture}" alt="{picture}" /> <div><h3>{name}, {location}</h3><h4>{company}</h4></div>' ,
-		speakerDetail: '{name}<p>{bio}</p>',  	
-		sessionItem: '{title} <p>{speakers}</p>',
-		sessionDetail: '{title}<p>{abstract}</p>'
+        speakerItem: speakerListTpl ,
+				speakerDetail: speakerDetailTpl,  	
+				sessionItem: sessionItemTpl,
+				sessionDetail: sessionDetailTpl
 });
+
+
+
