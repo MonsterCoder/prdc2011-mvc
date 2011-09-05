893f06a6ed3c8283f121fa38468ee9b0ef9797b4
MonsterCoder <monstercoder@gmail.com>
2011-09-05 13:58:01 -0500

    added landing pic

--- app/views/Home/home.js
+++ app/views/Home/home.js
@@ -1,5 +1,6 @@
 prdc.views.home = Ext.extend(Ext.Panel, {
-	html: "<iframe frameborder='0' src='http://prdc.herokuapp.com/' width='100%' height='100%' id='iframe-external'></iframe>",
+	// html: "<iframe frameborder='0' src='http://prdc.herokuapp.com/' width='100%' height='100%' id='iframe-external'></iframe>",
+	html: '<div class="landing_page" style="background-image: url(lib/resource/landing.jpg)"></div>'
 });
 
  Ext.reg('home', prdc.views.home ); 
