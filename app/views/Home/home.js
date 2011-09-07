f239c48e3fe5ebb85dd4ed01a533c3ef8018cf59
MonsterCoder <monstercoder@gmail.com>
2011-09-07 16:31:48 -0500

    ADD back home image

--- app/views/Home/home.js
+++ app/views/Home/home.js
@@ -1,6 +1,6 @@
 prdc.views.home = Ext.extend(Ext.Panel, {
 	// html: "<iframe frameborder='0' src='http://prdc.herokuapp.com/' width='100%' height='100%' id='iframe-external'></iframe>",
-	html: ' I see you!<div class="landing_page" style="background-image: url(http://prdc.herokuapp.com/assets/speakers/amirbarylko.jpg)"></div>'
+	html: '<div class="landing_page"> <img src="lib/resource/landing.jpg"></div>'
 });
 
  Ext.reg('home', prdc.views.home ); 
